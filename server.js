a8ba958ee82075f2d276454762a4e1ba3b13903a
Jason Day <jason.day.blue@gmail.com>
2022-01-19 23:07:34 -0600

    changed port to 3000

--- server.js
+++ server.js
@@ -58,4 +58,4 @@ app.post("/api/notes", (req, res) => {
 	}
 });
 
-app.listen(3001, () => console.log("Express Server on port 3000!"));
+app.listen(3000, () => console.log("Express Server on port 3000!"));
